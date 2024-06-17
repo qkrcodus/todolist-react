977e380191892131045ee7d901611509a5d6de13
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-06-17 11:31:25 +0900

    feat: add TodoHead 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import {createGlobalStyle} from 'styled-components';
 import './App.css'
 import { TodoTemplate } from './pages/main/TodoTemplate';
+import { TodoHead } from './pages/main/TodoHead';
 
+//styled component 를 만들고 이를 함수형 컴포넌트에 렌더링해준다. 
 const GlobalStyle=createGlobalStyle`
   body{
   background: #e9ecef;
-  display:flex;
-  justify-items:center;
-  align-items:center;
 }`
 
 function App() {
   return (
    <div>
     <GlobalStyle/>
-    <TodoTemplate/>
+    <TodoTemplate>
+    <TodoHead/>
+    </TodoTemplate>
    </div>
   )
 }
--- src/pages/main/TodoTemplate.jsx
+++ src/pages/main/TodoTemplate.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+
 const TodoTemplateBlock=styled.div`
 width: 512px;
 height: 768px;
-display: flex;
-flex-direction: column;
+margin: 0 auto;
 background: white;
 border-radius:20px;
 
 `
-export const TodoTemplate = () => {
+export const TodoTemplate = ({children}) => {
   return (
     <TodoTemplateBlock>
-      
+      {children}
     </TodoTemplateBlock>
   )
 }
