a3044c95a061497d0daa93b4a4d626ecc94d9532
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-06-21 15:04:08 +0900

    add: TodoCreate 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { TodoTemplate } from './pages/main/TodoTemplate';
 import { TodoHead } from './pages/main/TodoHead';
 import {TodoList} from './pages/main/TodoList';
 import { TodoItem } from './pages/main/TodoItem';
+import { TodoCreate } from './pages/main/TodoCreate';
 
 //styled component 를 만들고 이를 함수형 컴포넌트에 렌더링해준다. 
 const GlobalStyle=createGlobalStyle`
@@ -20,6 +21,7 @@ function App() {
     <TodoHead/>
     <TodoList/>
     <TodoItem/>
+    <TodoCreate/>
     </TodoTemplate>
    </div>
   )
--- src/pages/main/TodoItem.jsx
+++ src/pages/main/TodoItem.jsx
@@ -4,7 +4,8 @@ import { FaRegCircle } from "react-icons/fa";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 
-// 선언이 먼저 되어있어야 한다.
+//변수 호이스팅: 선언은 호이스팅 되지만 할당은 되지 않는다
+//함수 호이스팅: 함수 전체가 호이스팅 된다.
 const Remove = styled.div`
   display: flex;
   position: absolute;
@@ -19,8 +20,8 @@ const Remove = styled.div`
   display: none;
 `;
 
-// 토글 기능을 넣을거라. 생소한 styled 컴포넌트
-// Component Selector라는 기능으로 TodoItemBlock위에 커서가 있을때, Remove 컴포넌트를 보여준다.
+//토글 기능을 넣을거라. 생소한 styled 컴포넌트
+//Component Selector라는 기능으로 TodoItemBlock위에 커서가 있을때, Remove 컴포넌트를 보여준다.
 //styled component는 상위 컴포넌트 조건에따라 자신의 css,
 //자신의 조건에 따라 하위 컴포넌트의 css를 바꿀 수 있다
 const TodoItemBlock=styled.div`
@@ -36,7 +37,7 @@ const TodoItemBlock=styled.div`
     }
   }
 
-`
+` 
 
 const Text = styled.div`
   flex: 1;
@@ -50,10 +51,10 @@ const Text = styled.div`
     `}
 `;
 // done 이라는 prop에 따라 조건부로 다른 스타일이나 컴포넌트를 렌더링
-export const TodoItem = ({id,done,text}) => {
+export const TodoItem = ({done,text}) => {
   return (
     <TodoItemBlock>
-      {/* 삼항 연산자를 이용하여 props인  */}
+      {/* 삼항 연산자를 이용하여 props인 */}
        <div>
         {done ? <FaRegCheckCircle/> :<FaRegCircle/>}
        </div>
