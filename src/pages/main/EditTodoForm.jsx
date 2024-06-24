f8ee6fd836135da73f26f56f8afb1e059faf3dd4
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-06-24 10:00:20 +0900

    add : emoji 수정폼 추가

--- src/pages/main/EditTodoForm.jsx
+++ src/pages/main/EditTodoForm.jsx
@@ -2,16 +2,24 @@ import React, {useState} from 'react'
 
 export const EditTodoForm = ({editTodo, task}) => {
     const [value, setValue] = useState(task.task);
+    const [emoji, setEmoji] = useState(task.emoji); // 이모지 현재 상태 추가
 
     const handleSubmit = (e) => {
       // 폼 제출시 페이지 새로고침 방지
         e.preventDefault();
         // edit todo
-        editTodo(value, task.id);
+        editTodo({ task: value, emoji: emoji }, task.id);
       };
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
     <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='수정할 내용을 입력하세요' />
+    <input
+          type="text"
+          value={emoji}
+          onChange={(e) => setEmoji(e.target.value)}
+          className="todo-input"
+          placeholder='이모지를 입력하세요'
+            />
     <button type="submit" className='todo-btn'>수정완료</button>
   </form>
   )
